e8eabd019c5a616cfde1b8b6866973032a354124
Bikash Das <bikashdas@Bikashs-MacBook-Pro.local>
2025-11-14 09:30:17 +0530

    fix: passing skills now

--- candidates/serializers.py
+++ candidates/serializers.py
@@ -4,4 +4,18 @@ from .models import Candidate
 class CandidateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidate
-        fields = '__all__'
+        fields = [
+            'id',
+            'name',
+            'email',
+            'phone',
+            'employer',
+            'designation',
+            'skills',
+            'resume',
+            'aadhar_card',
+            'pan_card',
+            'document_request_message',
+            'created_at',
+            'updated_at'
+        ]
